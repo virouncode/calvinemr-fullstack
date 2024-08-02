6baa1dda74e12bb22a50b3815ad8701845e98014
Viroun KATTYGNARATH <virounkattygnarath@Ordinateur-portable-de-Viroun.local>
2024-08-03 01:03:52 +0200

    heroku config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      inject: {
-        inject: {
-          inject: `<script>
-          document.title = "${import.meta.env.VITE_CLINIC_NAME}";
-        </script>`,
-        },
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
