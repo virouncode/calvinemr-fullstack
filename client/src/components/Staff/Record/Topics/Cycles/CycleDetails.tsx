2ed6b89368b005d3029068d5e07e169c378772b2
Viroun KATTYGNARATH <virounk@gmail.com>
2025-06-16 16:03:04 +0200

    cycle duplication bug if careElementsData error

--- client/src/components/Staff/Record/Topics/Cycles/CycleDetails.tsx
+++ client/src/components/Staff/Record/Topics/Cycles/CycleDetails.tsx
@@ -54,12 +54,15 @@ const CycleDetails = ({
   }, [cycleToShow]);
 
   const handleSave = async () => {
+    if (progress) return;
+    setErrMsg("");
     try {
       await cycleSchema.validate(itemInfos);
     } catch (err) {
       if (err instanceof Error) setErrMsg(err.message);
       return;
     }
+    setProgress(true);
     const cycleToPut: CycleType = {
       ...(itemInfos as CycleType),
       updates: [
@@ -70,155 +73,161 @@ const CycleDetails = ({
       events: itemInfos.events?.map(({ id, ...event }) => event) ?? [],
       notes: itemInfos.notes?.map(({ id, ...note }) => note) ?? [],
     };
-    setProgress(true);
+
     topicPut.mutate(cycleToPut, {
-      onError: () => setProgress(false),
+      onSuccess: async () => {
+        try {
+          const careElementsDatas: CareElementType = (
+            await xanoGet("/care_elements_of_patient", "staff", {
+              patient_id: patientId,
+              page: 1,
+            })
+          ).items?.[0];
+          const regex = /^\d+(\.\d{0,2})?$/;
+          if (careElementsDatas) {
+            for (const event of itemInfos.events ?? []) {
+              if (event.e2) {
+                if (!regex.test(event.e2.toString())) {
+                  setErrMsg("Invalid E2 value. Please enter a valid number.");
+                  setProgress(false);
+                  return;
+                }
+                const e2Data = careElementsDatas?.E2.find(
+                  (data: { E2: string; Date: number; E2Unit: "pmol/L" }) =>
+                    data.Date === event.date
+                );
+                if (e2Data) {
+                  e2Data.E2 = event.e2;
+                } else {
+                  careElementsDatas?.E2.push({
+                    Date: event.date as number,
+                    E2: event.e2,
+                    E2Unit: "pmol/L",
+                  });
+                }
+              }
+              if (event.lh) {
+                if (!regex.test(event.lh.toString())) {
+                  setErrMsg("Invalid LH value. Please enter a valid number.");
+                  setProgress(false);
+                  return;
+                }
+                const lhData = careElementsDatas?.LH.find(
+                  (data: { LH: string; Date: number; LHUnit: "IU/L" }) =>
+                    data.Date === event.date
+                );
+                if (lhData) {
+                  lhData.LH = event.lh;
+                } else {
+                  careElementsDatas?.LH.push({
+                    Date: event.date as number,
+                    LH: event.lh,
+                    LHUnit: "IU/L",
+                  });
+                }
+              }
+              if (event.p4) {
+                if (!regex.test(event.p4.toString())) {
+                  setErrMsg("Invalid P4 value. Please enter a valid number.");
+                  setProgress(false);
+                  return;
+                }
+                const p4Data = careElementsDatas?.P4.find(
+                  (data: { P4: string; Date: number; P4Unit: "ng/mL" }) =>
+                    data.Date === event.date
+                );
+                if (p4Data) {
+                  p4Data.P4 = event.p4;
+                } else {
+                  careElementsDatas?.P4.push({
+                    Date: event.date as number,
+                    P4: event.p4,
+                    P4Unit: "ng/mL",
+                  });
+                }
+              }
+            }
+            const careElementsToPut = {
+              ...careElementsDatas,
+              updates: [
+                ...(careElementsDatas?.updates ?? []),
+                { updated_by_id: user.id, date_updated: nowTZTimestamp() },
+              ],
+            };
+            await xanoPut(
+              `/care_elements/${careElementsDatas.id}`,
+              "staff",
+              careElementsToPut
+            );
+            socket?.emit("message", { key: ["CARE ELEMENTS", patientId] });
+            setProgress(false);
+            setShow(false);
+          } else {
+            if (
+              itemInfos.events &&
+              itemInfos.events.length &&
+              itemInfos.events.some(
+                (event) =>
+                  (event.e2 && !regex.test(event.e2)) ||
+                  (event.lh && !regex.test(event.lh)) ||
+                  (event.p4 && !regex.test(event.p4))
+              )
+            ) {
+              setErrMsg(
+                "Invalid E2, LH or P4 value. Please enter a valid number."
+              );
+              setProgress(false);
+              return;
+            }
+            const careElementsToPost: Partial<CareElementType> = {
+              patient_id: patientId,
+              date_created: nowTZTimestamp(),
+              created_by_id: user.id,
+              E2: itemInfos.events
+                ? itemInfos.events
+                    .filter((event) => event.e2)
+                    .map((event) => ({
+                      Date: event.date as number,
+                      E2: event.e2,
+                      E2Unit: "pmol/L",
+                    }))
+                : [],
+              LH: itemInfos.events
+                ? itemInfos.events
+                    .filter((event) => event.lh)
+                    .map((event) => ({
+                      Date: event.date as number,
+                      LH: event.lh,
+                      LHUnit: "IU/L",
+                    }))
+                : [],
+              P4: itemInfos.events
+                ? itemInfos.events
+                    ?.filter((event) => event.p4)
+                    .map((event) => ({
+                      Date: event.date as number,
+                      P4: event.p4,
+                      P4Unit: "ng/mL",
+                    }))
+                : [],
+            };
+            await xanoPost("/care_elements", "staff", careElementsToPost);
+            socket?.emit("message", { key: ["CARE ELEMENTS", patientId] });
+            setProgress(false);
+            setShow(false);
+          }
+        } catch (err) {
+          if (err instanceof Error)
+            setErrMsg(`Unable to save cycle: ${err.message}`);
+          setProgress(false);
+        }
+      },
+      onError: (error) => {
+        setErrMsg(`Unable to save cycle: ${error.message}`);
+        setProgress(false);
+      },
     });
     //post e2, lh, p4 to Care elements
-
-    try {
-      const careElementsDatas: CareElementType = (
-        await xanoGet("/care_elements_of_patient", "staff", {
-          patient_id: patientId,
-          page: 1,
-        })
-      ).items?.[0];
-      const regex = /^\d+(\.\d{0,2})?$/;
-      if (careElementsDatas) {
-        for (const event of itemInfos.events ?? []) {
-          if (event.e2) {
-            if (!regex.test(event.e2.toString())) {
-              setErrMsg("Invalid E2 value. Please enter a valid number.");
-              setProgress(false);
-              return;
-            }
-            const e2Data = careElementsDatas?.E2.find(
-              (data: { E2: string; Date: number; E2Unit: "pmol/L" }) =>
-                data.Date === event.date
-            );
-            if (e2Data) {
-              e2Data.E2 = event.e2;
-            } else {
-              careElementsDatas?.E2.push({
-                Date: event.date as number,
-                E2: event.e2,
-                E2Unit: "pmol/L",
-              });
-            }
-          }
-          if (event.lh) {
-            if (!regex.test(event.lh.toString())) {
-              setErrMsg("Invalid LH value. Please enter a valid number.");
-              setProgress(false);
-              return;
-            }
-            const lhData = careElementsDatas?.LH.find(
-              (data: { LH: string; Date: number; LHUnit: "IU/L" }) =>
-                data.Date === event.date
-            );
-            if (lhData) {
-              lhData.LH = event.lh;
-            } else {
-              careElementsDatas?.LH.push({
-                Date: event.date as number,
-                LH: event.lh,
-                LHUnit: "IU/L",
-              });
-            }
-          }
-          if (event.p4) {
-            if (!regex.test(event.p4.toString())) {
-              setErrMsg("Invalid P4 value. Please enter a valid number.");
-              setProgress(false);
-              return;
-            }
-            const p4Data = careElementsDatas?.P4.find(
-              (data: { P4: string; Date: number; P4Unit: "ng/mL" }) =>
-                data.Date === event.date
-            );
-            if (p4Data) {
-              p4Data.P4 = event.p4;
-            } else {
-              careElementsDatas?.P4.push({
-                Date: event.date as number,
-                P4: event.p4,
-                P4Unit: "ng/mL",
-              });
-            }
-          }
-        }
-        const careElementsToPut = {
-          ...careElementsDatas,
-          updates: [
-            ...(careElementsDatas?.updates ?? []),
-            { updated_by_id: user.id, date_updated: nowTZTimestamp() },
-          ],
-        };
-        await xanoPut(
-          `/care_elements/${careElementsDatas.id}`,
-          "staff",
-          careElementsToPut
-        );
-        socket?.emit("message", { key: ["CARE ELEMENTS", patientId] });
-        setProgress(false);
-        setShow(false);
-      } else {
-        if (
-          itemInfos.events &&
-          itemInfos.events.length &&
-          itemInfos.events.some(
-            (event) =>
-              (event.e2 && !regex.test(event.e2)) ||
-              (event.lh && !regex.test(event.lh)) ||
-              (event.p4 && !regex.test(event.p4))
-          )
-        ) {
-          setErrMsg("Invalid E2, LH or P4 value. Please enter a valid number.");
-          setProgress(false);
-          return;
-        }
-        const careElementsToPost: Partial<CareElementType> = {
-          patient_id: patientId,
-          date_created: nowTZTimestamp(),
-          created_by_id: user.id,
-          E2: itemInfos.events
-            ? itemInfos.events
-                .filter((event) => event.e2)
-                .map((event) => ({
-                  Date: event.date as number,
-                  E2: event.e2,
-                  E2Unit: "pmol/L",
-                }))
-            : [],
-          LH: itemInfos.events
-            ? itemInfos.events
-                .filter((event) => event.lh)
-                .map((event) => ({
-                  Date: event.date as number,
-                  LH: event.lh,
-                  LHUnit: "IU/L",
-                }))
-            : [],
-          P4: itemInfos.events
-            ? itemInfos.events
-                ?.filter((event) => event.p4)
-                .map((event) => ({
-                  Date: event.date as number,
-                  P4: event.p4,
-                  P4Unit: "ng/mL",
-                }))
-            : [],
-        };
-        await xanoPost("/care_elements", "staff", careElementsToPost);
-        socket?.emit("message", { key: ["CARE ELEMENTS", patientId] });
-        setProgress(false);
-        setShow(false);
-      }
-    } catch (err) {
-      if (err instanceof Error)
-        setErrMsg(`Unable to save cycle: ${err.message}`);
-      setProgress(false);
-    }
   };
   const handleCancel = async () => {
     if (
--- client/src/components/Staff/Record/Topics/Cycles/CycleForm.tsx
+++ client/src/components/Staff/Record/Topics/Cycles/CycleForm.tsx
@@ -52,6 +52,7 @@ const CycleForm = ({
   );
 
   const handleSubmit = async () => {
+    if (progress) return;
     setErrMsg("");
     try {
       await cycleSchema.validate(formDatas);
@@ -59,159 +60,167 @@ const CycleForm = ({
       if (err instanceof Error) setErrMsg(err.message);
       return;
     }
+    setProgress(true);
     const cycleToPost: Partial<CycleType> = {
       ...formDatas,
       date_created: nowTZTimestamp(),
       created_by_id: user.id,
       cycle_nbr: `${demographicsInfos.ChartNumber}-${formDatas.cycle_nbr}`,
     };
-    setProgress(true);
+
     topicPost.mutate(cycleToPost, {
-      onError: () => setProgress(false),
-    });
-    try {
-      const careElementsDatas: CareElementType = (
-        await xanoGet("/care_elements_of_patient", "staff", {
-          patient_id: patientId,
-          page: 1,
-        })
-      ).items?.[0];
-      const regex = /^\d+(\.\d{0,2})?$/;
-      if (careElementsDatas) {
-        for (const event of formDatas.events ?? []) {
-          if (event.e2) {
-            if (!regex.test(event.e2.toString())) {
-              setErrMsg("Invalid E2 value. Please enter a valid number.");
+      onSuccess: async () => {
+        try {
+          const careElementsDatas: CareElementType = (
+            await xanoGet("/care_elements_of_patient", "staff", {
+              patient_id: patientId,
+              page: 1,
+            })
+          ).items?.[0];
+          const regex = /^\d+(\.\d{0,2})?$/;
+          if (careElementsDatas) {
+            for (const event of formDatas.events ?? []) {
+              if (event.e2) {
+                if (!regex.test(event.e2.toString())) {
+                  setErrMsg("Invalid E2 value. Please enter a valid number.");
+                  setProgress(false);
+                  return;
+                }
+                const e2Data = careElementsDatas?.E2.find(
+                  (data: { E2: string; Date: number; E2Unit: "pmol/L" }) =>
+                    data.Date === event.date
+                );
+                if (e2Data) {
+                  e2Data.E2 = event.e2;
+                } else {
+                  careElementsDatas?.E2.push({
+                    Date: event.date as number,
+                    E2: event.e2,
+                    E2Unit: "pmol/L",
+                  });
+                }
+              }
+              if (event.lh) {
+                if (!regex.test(event.lh.toString())) {
+                  setErrMsg("Invalid LH value. Please enter a valid number.");
+                  setProgress(false);
+                  return;
+                }
+                const lhData = careElementsDatas?.LH.find(
+                  (data: { LH: string; Date: number; LHUnit: "IU/L" }) =>
+                    data.Date === event.date
+                );
+                if (lhData) {
+                  lhData.LH = event.lh;
+                } else {
+                  careElementsDatas?.LH.push({
+                    Date: event.date as number,
+                    LH: event.lh,
+                    LHUnit: "IU/L",
+                  });
+                }
+              }
+              if (event.p4) {
+                if (!regex.test(event.p4.toString())) {
+                  setErrMsg("Invalid P4 value. Please enter a valid number.");
+                  setProgress(false);
+                  return;
+                }
+                const p4Data = careElementsDatas?.P4.find(
+                  (data: { P4: string; Date: number; P4Unit: "ng/mL" }) =>
+                    data.Date === event.date
+                );
+                if (p4Data) {
+                  p4Data.P4 = event.p4;
+                } else {
+                  careElementsDatas?.P4.push({
+                    Date: event.date as number,
+                    P4: event.p4,
+                    P4Unit: "ng/mL",
+                  });
+                }
+              }
+            }
+            const careElementsToPut = {
+              ...careElementsDatas,
+              updates: [
+                ...(careElementsDatas?.updates ?? []),
+                { updated_by_id: user.id, date_updated: nowTZTimestamp() },
+              ],
+            };
+            await xanoPut(
+              `/care_elements/${careElementsDatas.id}`,
+              "staff",
+              careElementsToPut
+            );
+            socket?.emit("message", { key: ["CARE ELEMENTS", patientId] });
+            setProgress(false);
+            setAddVisible(false);
+          } else {
+            if (
+              formDatas.events &&
+              formDatas.events.length &&
+              formDatas.events?.some(
+                (event) =>
+                  (event.e2 && !regex.test(event.e2)) ||
+                  (event.lh && !regex.test(event.lh)) ||
+                  (event.p4 && !regex.test(event.p4))
+              )
+            ) {
+              setErrMsg(
+                "Invalid E2, LH or P4 value. Please enter a valid number."
+              );
               setProgress(false);
               return;
             }
-            const e2Data = careElementsDatas?.E2.find(
-              (data: { E2: string; Date: number; E2Unit: "pmol/L" }) =>
-                data.Date === event.date
-            );
-            if (e2Data) {
-              e2Data.E2 = event.e2;
-            } else {
-              careElementsDatas?.E2.push({
-                Date: event.date as number,
-                E2: event.e2,
-                E2Unit: "pmol/L",
-              });
-            }
+            const careElementsToPost: Partial<CareElementType> = {
+              patient_id: patientId,
+              date_created: nowTZTimestamp(),
+              created_by_id: user.id,
+              E2: formDatas.events
+                ? formDatas.events
+                    .filter((event) => event.e2)
+                    .map((event) => ({
+                      Date: event.date as number,
+                      E2: event.e2,
+                      E2Unit: "pmol/L",
+                    }))
+                : [],
+              LH: formDatas.events
+                ? formDatas.events
+                    .filter((event) => event.lh)
+                    .map((event) => ({
+                      Date: event.date as number,
+                      LH: event.lh,
+                      LHUnit: "IU/L",
+                    }))
+                : [],
+              P4: formDatas.events
+                ? formDatas.events
+                    ?.filter((event) => event.p4)
+                    .map((event) => ({
+                      Date: event.date as number,
+                      P4: event.p4,
+                      P4Unit: "ng/mL",
+                    }))
+                : [],
+            };
+            await xanoPost("/care_elements", "staff", careElementsToPost);
+            socket?.emit("message", { key: ["CARE ELEMENTS", patientId] });
+            setProgress(false);
+            setAddVisible(false);
           }
-          if (event.lh) {
-            if (!regex.test(event.lh.toString())) {
-              setErrMsg("Invalid LH value. Please enter a valid number.");
-              setProgress(false);
-              return;
-            }
-            const lhData = careElementsDatas?.LH.find(
-              (data: { LH: string; Date: number; LHUnit: "IU/L" }) =>
-                data.Date === event.date
-            );
-            if (lhData) {
-              lhData.LH = event.lh;
-            } else {
-              careElementsDatas?.LH.push({
-                Date: event.date as number,
-                LH: event.lh,
-                LHUnit: "IU/L",
-              });
-            }
-          }
-          if (event.p4) {
-            if (!regex.test(event.p4.toString())) {
-              setErrMsg("Invalid P4 value. Please enter a valid number.");
-              setProgress(false);
-              return;
-            }
-            const p4Data = careElementsDatas?.P4.find(
-              (data: { P4: string; Date: number; P4Unit: "ng/mL" }) =>
-                data.Date === event.date
-            );
-            if (p4Data) {
-              p4Data.P4 = event.p4;
-            } else {
-              careElementsDatas?.P4.push({
-                Date: event.date as number,
-                P4: event.p4,
-                P4Unit: "ng/mL",
-              });
-            }
-          }
-        }
-        const careElementsToPut = {
-          ...careElementsDatas,
-          updates: [
-            ...(careElementsDatas?.updates ?? []),
-            { updated_by_id: user.id, date_updated: nowTZTimestamp() },
-          ],
-        };
-        await xanoPut(
-          `/care_elements/${careElementsDatas.id}`,
-          "staff",
-          careElementsToPut
-        );
-        socket?.emit("message", { key: ["CARE ELEMENTS", patientId] });
-        setProgress(false);
-        setAddVisible(false);
-      } else {
-        if (
-          formDatas.events &&
-          formDatas.events.length &&
-          formDatas.events?.some(
-            (event) =>
-              (event.e2 && !regex.test(event.e2)) ||
-              (event.lh && !regex.test(event.lh)) ||
-              (event.p4 && !regex.test(event.p4))
-          )
-        ) {
-          setErrMsg("Invalid E2, LH or P4 value. Please enter a valid number.");
+        } catch (err) {
+          if (err instanceof Error)
+            setErrMsg(`Unable to save cycle: ${err.message}`);
           setProgress(false);
-          return;
         }
-        const careElementsToPost: Partial<CareElementType> = {
-          patient_id: patientId,
-          date_created: nowTZTimestamp(),
-          created_by_id: user.id,
-          E2: formDatas.events
-            ? formDatas.events
-                .filter((event) => event.e2)
-                .map((event) => ({
-                  Date: event.date as number,
-                  E2: event.e2,
-                  E2Unit: "pmol/L",
-                }))
-            : [],
-          LH: formDatas.events
-            ? formDatas.events
-                .filter((event) => event.lh)
-                .map((event) => ({
-                  Date: event.date as number,
-                  LH: event.lh,
-                  LHUnit: "IU/L",
-                }))
-            : [],
-          P4: formDatas.events
-            ? formDatas.events
-                ?.filter((event) => event.p4)
-                .map((event) => ({
-                  Date: event.date as number,
-                  P4: event.p4,
-                  P4Unit: "ng/mL",
-                }))
-            : [],
-        };
-        await xanoPost("/care_elements", "staff", careElementsToPost);
-        socket?.emit("message", { key: ["CARE ELEMENTS", patientId] });
+      },
+      onError: (error) => {
+        setErrMsg(`Unable to save cycle: ${error.message}`);
         setProgress(false);
-        setAddVisible(false);
-      }
-    } catch (err) {
-      if (err instanceof Error)
-        setErrMsg(`Unable to save cycle: ${err.message}`);
-      setProgress(false);
-    }
+      },
+    });
   };
 
   const handleClose = async () => {
