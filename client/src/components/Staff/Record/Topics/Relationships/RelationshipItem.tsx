64dd8c23894bcb738836719660b702112b7cd5e5
Viroun KATTYGNARATH <virounk@gmail.com>
2025-07-11 23:31:51 +0200

    relationships

--- client/src/components/Staff/Record/Topics/Relationships/RelationshipItem.tsx
+++ client/src/components/Staff/Record/Topics/Relationships/RelationshipItem.tsx
@@ -57,6 +57,7 @@ const RelationshipItem = ({
   const [patientSelected, setPatientSelected] = useState<
     DemographicsType | undefined
   >();
+  console.log("item", item);
 
   const fakewindowRoot = document.getElementById("fake-window");
 
